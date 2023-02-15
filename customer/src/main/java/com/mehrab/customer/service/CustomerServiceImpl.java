6f38f83c2bb467f6397913284a2f9ef22d24403b
Mehrab <mehrab.kor@gmail.com>
2023-02-15 11:19:19 +0330

    spring-cloud -> network discovery(eureka-server)

--- customer/src/main/java/com/mehrab/customer/service/CustomerServiceImpl.java
+++ customer/src/main/java/com/mehrab/customer/service/CustomerServiceImpl.java
@@ -23,7 +23,7 @@ public class CustomerServiceImpl implements CustomerService {
         // todo: check if email not taken
         customerRepository.saveAndFlush(customer); // we can access id of saved customer
         // todo: check if fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
         if(fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
--- fraud/src/main/java/com/mehrab/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/mehrab/fraud/controller/FraudController.java
@@ -4,11 +4,13 @@ package com.mehrab.fraud.controller;
 import com.mehrab.fraud.payload.response.FraudCheckResponse;
 import com.mehrab.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 @AllArgsConstructor
 public class FraudController {
     private final FraudCheckService fraudCheckService;
@@ -16,6 +18,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         var isFraudulentCustomer =  fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for cutomer id {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
