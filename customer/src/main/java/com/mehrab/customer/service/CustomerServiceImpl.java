91803078531b52834fb9dc64e51c78b32ff79109
Mehrab <mehrab.kor@gmail.com>
2023-02-20 14:30:55 +0330

    add Zipkin

--- customer/src/main/java/com/mehrab/customer/service/CustomerServiceImpl.java
+++ customer/src/main/java/com/mehrab/customer/service/CustomerServiceImpl.java
@@ -2,6 +2,8 @@ package com.mehrab.customer.service;
 
 import com.mehrab.clients.fraud.FraudCheckResponse;
 import com.mehrab.clients.fraud.FraudClient;
+import com.mehrab.clients.notification.NotificationClient;
+import com.mehrab.clients.notification.NotificationRequest;
 import com.mehrab.customer.model.Customer;
 import com.mehrab.customer.payload.request.CustomerRegistrationRequest;
 import com.mehrab.customer.repository.CustomerRepository;
@@ -12,18 +14,22 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @AllArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+//    private final RestTemplate restTemplate;
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
     @Override
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email()).build();
+
         // todo: check if email valid
+
         // todo: check if email not taken
         customerRepository.saveAndFlush(customer); // we can access id of saved customer
+
         // todo: check if fraudster
 //        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
         FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
@@ -31,7 +37,8 @@ public class CustomerServiceImpl implements CustomerService {
             throw new IllegalStateException("fraudster");
         }
 
-        // todo: send notification
+        // todo: make it async send notification. i.e add to queue
+//        notificationClient.sendNotification(new NotificationRequest(customer.getId(), customer.getEmail(), String.format("Hi %s, Welcome to my miroservice", customer.getFirstName())));
 
     }
 }
